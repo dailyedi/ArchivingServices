53cc124c670ef72a24118870c4130cfe0a6b9624
MuhammadFareedDaily <muhammad.fareed@dailyedi.com>
2021-09-30 11:08:19 +0200

    Get latest from testing.

---
+++ ArchivingServices/Structure/FileMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArchivingServices.Structure
+{
+    public class FileMetadata
+    {
+        public long CompressedLength { get; set; }
+        public string FullName { get; set; }
+        public long Length { get; set; }
+        public DateTime LastWriteTime { get; set; }
+    }
+}
